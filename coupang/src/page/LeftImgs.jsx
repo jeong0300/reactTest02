b0ea18d76d075baf02161bcaffcd4d5b9b4f678b
jeong0300 <dltnwjd8898@naver.com>
2025-03-28 19:06:11 +0900

    test

--- coupang/src/page/LeftImgs.jsx
+++ coupang/src/page/LeftImgs.jsx
@@ -1,57 +1,45 @@
 import React, { useState, useEffect } from "react";
 import "../css/LeftImgs.css";
 
-import pinkBlue1 from "../image/pinkBlue1.jpg";
-import pinkBlue2 from "../image/pinkBlue2.jpg";
-import orangeBlue1 from "../image/orangeBlue1.jpg";
-import orangeBlue2 from "../image/orangBlue2.jpg";
-import choco1 from "../image/choco1.jpg";
-import choco2 from "../image/choco2.jpg";
-import orangeBrown1 from "../image/orangeBrown1.jpg";
-import orangeBrown2 from "../image/orangeBrown2.jpg";
-import pink1 from "../image/pink1.jpg";
-import pink2 from "../image/pink2.jpg";
-import pinkBrown1 from "../image/pinkBrown1.jpg";
-import pinkBrown2 from "../image/pinkBrown2.jpg";
-
 const LeftImgs = (props) => {
-  const { product, setProduct } = props;
+  const { product, setProduct, hoveredProduct, setHoveredProduct, images } =
+    props;
 
-  const [hoveredProduct, setHoveredProduct] = useState(product);
+  const selectedImages = images[product] || [];
 
-  const handleImageClick = (productName) => {
-    setProduct(hoveredProduct);
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setImageIndex(index);
+    setProduct(product);
+    setHoveredProduct(product);
   };
 
-  const items = {
-    핑크블루: [pinkBlue1, pinkBlue2],
-    오렌지블루: [orangeBlue1, orangeBlue2],
-    초코: [choco1, choco2],
-    오렌지브라운: [orangeBrown1, orangeBrown2],
-    핑크: [pink1, pink2],
-    핑크브라운: [pinkBrown1, pinkBrown2],
+  const handleMouseOver = (index) => {
+    setImageIndex(index);
+    setHoveredProduct(product);
   };
+
+  useEffect(() => {
+    if (!hoveredProduct) {
+      setImageIndex(0);
+    }
+  }, [product, hoveredProduct]);
 
   return (
-    <>
-      <div className="flexItem">
-        <div className="rightItem">
-          <img
-            className="MainImg"
-            src={items[hoveredProduct] || items[product]}
-            alt={product}
-          />
-        </div>
-        <div className="leftItems">
-          <LeftImgs
-            product={product}
-            setProduct={setProduct}
-            hoveredProduct={hoveredProduct}
-            setHoveredProduct={setHoveredProduct}
-          />
-        </div>
-      </div>
-    </>
+    <div className="itemBox">
+      {selectedImages.map((image, index) => (
+        <img
+          key={index}
+          className="leftImg"
+          src={image}
+          alt={`${product} image`}
+          onClick={() => handleClick(index)}
+          onMouseOver={() => handleMouseOver(index)}
+          onMouseOut={() => handleMouseOver(index)}
+        />
+      ))}
+    </div>
   );
 };
 
--- coupang/src/page/MainLeftPage.jsx
+++ coupang/src/page/MainLeftPage.jsx
@@ -3,33 +3,40 @@ import LeftImgs from "./LeftImgs";
 
 // 이미지
 import pinkBlue1 from "../image/pinkBlue1.jpg";
+import pinkBlue2 from "../image/pinkBlue2.jpg";
 import orangeBlue1 from "../image/orangeBlue1.jpg";
+import orangeBlue2 from "../image/orangBlue2.jpg";
 import choco1 from "../image/choco1.jpg";
+import choco2 from "../image/choco2.jpg";
 import orangeBrown1 from "../image/orangeBrown1.jpg";
+import orangeBrown2 from "../image/orangeBrown2.jpg";
 import pink1 from "../image/pink1.jpg";
+import pink2 from "../image/pink2.jpg";
 import pinkBrown1 from "../image/pinkBrown1.jpg";
+import pinkBrown2 from "../image/pinkBrown2.jpg";
 
 const MainLeftPage = (props) => {
   const { product, setProduct, hoveredProduct, setHoveredProduct } = props;
 
   const images = {
-    핑크블루: pinkBlue1,
-    오렌지블루: orangeBlue1,
-    초코: choco1,
-    오렌지브라운: orangeBrown1,
-    핑크: pink1,
-    핑크브라운: pinkBrown1,
+    핑크블루: [pinkBlue1, pinkBlue2],
+    오렌지블루: [orangeBlue1, orangeBlue2],
+    초코: [choco1, choco2],
+    오렌지브라운: [orangeBrown1, orangeBrown2],
+    핑크: [pink1, pink2],
+    핑크브라운: [pinkBrown1, pinkBrown2],
   };
+
+  const selectedProduct = hoveredProduct || product;
+  const selectedImage = images[selectedProduct]
+    ? images[selectedProduct][0]
+    : images[product][0];
 
   return (
     <>
       <div className="flexItem">
         <div className="rightItem">
-          <img
-            className="MainImg"
-            src={images[hoveredProduct] || images[product]}
-            alt={product}
-          />
+          <img className="MainImg" src={selectedImage} alt={selectedProduct} />
         </div>
         <div className="leftItems">
           <LeftImgs
@@ -37,6 +44,7 @@ const MainLeftPage = (props) => {
             setProduct={setProduct}
             hoveredProduct={hoveredProduct}
             setHoveredProduct={setHoveredProduct}
+            images={images}
           />
         </div>
       </div>
